d36e2da58b27f35fde116ae1bea2890180e61c81
Kody Horiuchi <111256751+mrhoriuchi@users.noreply.github.com>
2022-11-05 11:34:23 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+const $gifArea = $("#gifArea");
+const $searchInput = $("#inputGif");
+
+$("form").on("submit", async function (e) {
+  e.preventDefault();
+  let searchTerm = $searchInput.val();
+  $searchInput.val("");
+  const res = await axios.get("http://api.giphy.com/v1/gifs/search", {
+    params: {
+      q: searchTerm,
+      api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym",
+    },
+  });
+  addGif(res.data);
+});
+
+$("#btnRmv").on("click", function (e) {
+  e.preventDefault();
+  $gifArea.empty();
+});
+
+function addGif(res) {
+  let numGifs = res.data.length;
+  if (numGifs) {
+    let rdmIdx = Math.floor(Math.random() * numGifs);
+    let $newGif = $("<img>", {
+      src: res.data[rdmIdx].images.original.url,
+    });
+    $gifArea.append($newGif);
+  }
+}
